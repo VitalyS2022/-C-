eae6126c974a17ed45fe974f57c4984613892b8a
Vitaly <Vitaliy1999@mail.ru>
2022-06-28 15:38:22 +0600

    команды выгрузки в репозитарий

--- Program.cs
+++ Program.cs
@@ -1,1 +1,5 @@
-﻿Console.Write("Тренировка с .gitinore");
+﻿//git remote add origin https://github.com/VitalyS2022/SeminarsCsharp.git
+//git branch -M main
+//git push -u origin main
+
+Console.Write("Тренировка с .gitinore");
