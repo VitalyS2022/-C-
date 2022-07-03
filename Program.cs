74e45c3d81b45882802171bf1f3395e4dd399932
Vitaly <Vitaliy1999@mail.ru>
2022-07-03 17:42:39 +0600

    Начинаем работать

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿int a, a2, a3;
+
+Console.Write("Input a number 4: ");
+a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a number 5: ");
+a2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a number 6: ");
+a3 = Convert.ToInt32(Console.ReadLine());
+
+int[] array = { a, a2, a3 };
+
+int find = 5;
+
+int index = 0;
+
+while (index < 2)
+{
+    if (array[index] == 5)
+    {
+
+        //Console.WriteLine(index); // показывает номер индекса массива, покажет 1, так как считается от 0 
+        Console.WriteLine(array[index]); //показывает число какое находится под номером индекса
+        
+    }
+    //index = index + 1;
+    index++;
+}
+
