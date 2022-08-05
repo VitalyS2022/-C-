3c70b456c55d6744d0b0e634d45ba2837cf27275
Vitaly <Vitaliy1999@mail.ru>
2022-08-05 09:19:18 +0600

    выполнение задачи по контрольной работе

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿//Решение задачи по контрольной работе
+
+string[] array1 = new string[10] {"ACG6", "123", "F2G", "AQ", "12", "67", "890", "WERT", "A", "1"};
+string[] array2 = new string[array1.Length];
+int count = 0;
+
+void massive(string[] array1, string[] array2)
+{
+   
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+Console.Write("Вывод строк <= 3 символам: ");
+massive(array1, array2);
+PrintArray(array2);
